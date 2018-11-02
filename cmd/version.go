6cd5acb780245b19d9f5caa206aa9fe67e56f155
John Arundel <john@bitfieldconsulting.com>
2018-11-02 18:12:12 +0000

    Add 'version' command

---
+++ cmd/version.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var version = "0.7.0"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show version",
+	Long:  `Show uptimerobot client version`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("uptimerobot version", version)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+}
