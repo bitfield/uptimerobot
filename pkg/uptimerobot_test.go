8b6813e10e93f97eb62cedf1ea5674ffcf614636
John Arundel <john@bitfieldconsulting.com>
2019-08-12 18:28:12 +0100

    Extend and improve tests

--- pkg/uptimerobot_test.go
+++ pkg/uptimerobot_test.go
@@ -1,6 +1,7 @@
 package uptimerobot
 
 import (
+	"encoding/json"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -10,6 +11,41 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 )
+
+func TestMarshalMonitor(t *testing.T) {
+	t.Parallel()
+	m := Monitor{
+		ID:            777749809,
+		FriendlyName:  "Google",
+		URL:           "http://www.google.com",
+		Type:          MonitorType("HTTP"),
+		Port:          80,
+		AlertContacts: []string{"3", "5", "7"},
+	}
+	got, err := json.MarshalIndent(m, "", "  ")
+	if err != nil {
+		t.Error(err)
+	}
+	want, err := ioutil.ReadFile("testdata/marshal.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Convert the actual data and the expected data to maps, for ease of
+	// comparison
+	wantMap := map[string]interface{}{}
+	err = json.Unmarshal(want, &wantMap)
+	if err != nil {
+		t.Fatal(err)
+	}
+	gotMap := map[string]interface{}{}
+	err = json.Unmarshal(got, &gotMap)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !cmp.Equal(wantMap, gotMap) {
+		t.Error(cmp.Diff(wantMap, gotMap))
+	}
+}
 
 func TestUnmarshalMonitor(t *testing.T) {
 	t.Parallel()
@@ -34,20 +70,41 @@ func TestUnmarshalMonitor(t *testing.T) {
 	}
 
 }
+
 func TestNewMonitor(t *testing.T) {
 	t.Parallel()
 	client := New("dummy")
 	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "POST" {
+		if r.Method != http.MethodPost {
 			t.Errorf("want POST request, got %q", r.Method)
 		}
 		wantURL := "/v2/newMonitor"
 		if r.URL.EscapedPath() != wantURL {
 			t.Errorf("want %q, got %q", wantURL, r.URL.EscapedPath())
 		}
-		_, err := ioutil.ReadAll(r.Body)
+		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			t.Fatal(err)
+		}
+		r.Body.Close()
+		want, err := ioutil.ReadFile("testdata/requestNewMonitor.json")
+		if err != nil {
+			t.Fatal(err)
+		}
+		// Convert the received body and the expected body to maps, for
+		// ease of comparison
+		wantMap := map[string]interface{}{}
+		err = json.Unmarshal(want, &wantMap)
+		if err != nil {
+			t.Fatal(err)
+		}
+		bodyMap := map[string]interface{}{}
+		err = json.Unmarshal(body, &bodyMap)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !cmp.Equal(wantMap, bodyMap) {
+			t.Error(cmp.Diff(wantMap, bodyMap))
 		}
 		w.WriteHeader(http.StatusCreated)
 		data, err := os.Open("testdata/newMonitor.json")
@@ -61,9 +118,11 @@ func TestNewMonitor(t *testing.T) {
 	client.http = ts.Client()
 	client.URL = ts.URL
 	want := Monitor{
-		FriendlyName: "My test monitor",
-		URL:          "http://example.com",
-		Type:         MonitorType("HTTP"),
+		FriendlyName:  "My test monitor",
+		URL:           "http://example.com",
+		Type:          MonitorType("HTTP"),
+		Port:          80,
+		AlertContacts: []string{"3", "5", "7"},
 	}
 	got, err := client.NewMonitor(want)
 	if err != nil {
@@ -89,13 +148,20 @@ func TestGetAccountDetails(t *testing.T) {
 	defer ts.Close()
 	client.http = ts.Client()
 	client.URL = ts.URL
-	a, err := client.GetAccountDetails()
+	got, err := client.GetAccountDetails()
 	if err != nil {
 		t.Error(err)
 	}
-	wantEmail := "test@domain.com"
-	if a.Email != wantEmail {
-		t.Errorf("GetAccountDetails() => email %q, want %q", a.Email, wantEmail)
+	want := Account{
+		Email:           "test@domain.com",
+		MonitorLimit:    50,
+		MonitorInterval: 1,
+		UpMonitors:      1,
+		DownMonitors:    0,
+		PausedMonitors:  2,
+	}
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
 	}
 }
 
@@ -114,15 +180,28 @@ func TestGetAlertContacts(t *testing.T) {
 	defer ts.Close()
 	client.http = ts.Client()
 	client.URL = ts.URL
-	want := []string{"John Doe", "My Twitter"}
-	contacts, err := client.GetAlertContacts()
+	want := []AlertContact{
+		{
+			ID:           "0993765",
+			FriendlyName: "John Doe",
+			Type:         2,
+			Status:       1,
+			Value:        "johndoe@gmail.com",
+		},
+		{
+			ID:           "2403924",
+			FriendlyName: "My Twitter",
+			Type:         3,
+			Status:       0,
+			Value:        "sampleTwitterAccount",
+		},
+	}
+	got, err := client.GetAlertContacts()
 	if err != nil {
 		t.Error(err)
 	}
-	for i, m := range contacts {
-		if m.FriendlyName != want[i] {
-			t.Errorf("GetAlertContacts[%d] => %q, want %q", i, m.FriendlyName, want[i])
-		}
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
 	}
 }
 
@@ -141,13 +220,19 @@ func TestGetMonitorByID(t *testing.T) {
 	defer ts.Close()
 	client.http = ts.Client()
 	client.URL = ts.URL
-	var want int64 = 777749809
-	got, err := client.GetMonitorByID(want)
+	want := Monitor{
+		ID:           777749809,
+		FriendlyName: "Google",
+		URL:          "http://www.google.com",
+		Type:         MonitorType("HTTP"),
+		Port:         80,
+	}
+	got, err := client.GetMonitorByID(want.ID)
 	if err != nil {
 		t.Error(err)
 	}
-	if got.ID != want {
-		t.Errorf("GetMonitor() => ID %d, want %d", got.ID, want)
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
 	}
 }
 
@@ -166,20 +251,43 @@ func TestGetMonitors(t *testing.T) {
 	defer ts.Close()
 	client.http = ts.Client()
 	client.URL = ts.URL
-	want := []string{
-		"Google",
-		"My Web Page",
-		"My FTP Server",
-		"PortTest",
+	want := []Monitor{
+		{
+			ID:           777749809,
+			FriendlyName: "Google",
+			URL:          "http://www.google.com",
+			Type:         MonitorType("HTTP"),
+			Port:         80,
+		},
+		{
+			ID:           777712827,
+			FriendlyName: "My Web Page",
+			URL:          "http://mywebpage.com/",
+			Type:         MonitorType("HTTP"),
+		},
+		{
+			ID:           777559666,
+			FriendlyName: "My FTP Server",
+			URL:          "ftp.mywebpage.com",
+			Type:         MonitorType("port"),
+			SubType:      MonitorSubType("FTP (21)"),
+			Port:         21,
+		},
+		{
+			ID:           781397847,
+			FriendlyName: "PortTest",
+			URL:          "mywebpage.com",
+			Type:         MonitorType("port"),
+			SubType:      MonitorSubType("Custom Port"),
+			Port:         8000,
+		},
 	}
-	monitors, err := client.GetMonitors()
+	got, err := client.GetMonitors()
 	if err != nil {
 		t.Error(err)
 	}
-	for i, m := range monitors {
-		if m.FriendlyName != want[i] {
-			t.Errorf("GetMonitors[%d] => %q, want %q", i, m.FriendlyName, want[i])
-		}
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
 	}
 }
 
@@ -198,14 +306,20 @@ func TestGetMonitorsBySearch(t *testing.T) {
 	defer ts.Close()
 	client.http = ts.Client()
 	client.URL = ts.URL
-	want := "My Web Page"
-	monitors, err := client.GetMonitorsBySearch(want)
+	want := []Monitor{
+		{
+			ID:           777712827,
+			FriendlyName: "My Web Page",
+			URL:          "http://mywebpage.com/",
+			Type:         MonitorType("HTTP"),
+		},
+	}
+	got, err := client.GetMonitorsBySearch("My Web Page")
 	if err != nil {
 		t.Error(err)
 	}
-	got := monitors[0].FriendlyName
-	if got != want {
-		t.Errorf("GetMonitorBySearch(%q) => %q", want, got)
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
 	}
 }
 
@@ -231,8 +345,8 @@ func TestPauseMonitor(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	if got.ID != want.ID {
-		t.Errorf("PauseMonitor() => ID %d, want %d", got.ID, want.ID)
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
 	}
 }
 
@@ -258,8 +372,8 @@ func TestStartMonitor(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	if got.ID != want.ID {
-		t.Errorf("StartMonitor() => ID %d, want %d", got.ID, want.ID)
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
 	}
 }
 
@@ -279,16 +393,17 @@ func TestEnsureMonitor(t *testing.T) {
 	client.http = ts.Client()
 	client.URL = ts.URL
 	want := Monitor{
+		ID:           777712827,
 		FriendlyName: "My Web Page",
-		URL:          "http://mywebpage.com",
+		URL:          "http://mywebpage.com/",
 		Type:         MonitorType("HTTP"),
 	}
 	got, err := client.EnsureMonitor(want)
 	if err != nil {
 		t.Error(err)
 	}
-	if got.ID != 777712827 {
-		t.Errorf("EnsureMonitor() => ID %d, want 777712827", got.ID)
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
 	}
 }
 
@@ -308,28 +423,40 @@ func TestDeleteMonitor(t *testing.T) {
 	client.http = ts.Client()
 	client.URL = ts.URL
 	want := Monitor{
-		ID: 777810874,
+		ID:   777810874,
+		Type: MonitorType("HTTP"),
 	}
 	got, err := client.DeleteMonitor(want)
 	if err != nil {
 		t.Error(err)
 	}
-	if got.ID != want.ID {
-		t.Errorf("NewMonitor() => ID %d, want %d", got.ID, want.ID)
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
 	}
 }
 
-func TestBuildAlertContacts(t *testing.T) {
+func TestRenderMonitor(t *testing.T) {
 	t.Parallel()
-	contacts := []string{"2353888", "0132759"}
-	want := "2353888_0_0-0132759_0_0"
-	got := buildAlertContactList(contacts)
-	if got != want {
-		t.Errorf("buildAlertContacts() => %q, want %q", got, want)
+	input := Monitor{
+		ID:            777749809,
+		FriendlyName:  "Google",
+		URL:           "http://www.google.com",
+		Type:          MonitorType("HTTP"),
+		Port:          80,
+		AlertContacts: []string{"3", "5", "7"},
+	}
+	wantBytes, err := ioutil.ReadFile("testdata/monitor_template.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := string(wantBytes)
+	got := render(monitorTemplate, input)
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
 	}
 }
 
-func TestRender(t *testing.T) {
+func TestRenderAccount(t *testing.T) {
 	t.Parallel()
 	input := Account{
 		Email:           "j.random@example.com",
@@ -345,8 +472,8 @@ func TestRender(t *testing.T) {
 	}
 	want := string(wantBytes)
 	got := render(accountTemplate, input)
-	if got != want {
-		t.Errorf("render(%q) = %q, want %q", input, got, want)
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
 	}
 }
 
